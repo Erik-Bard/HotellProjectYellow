8fc351cd71d39406d2bdb7b4ce856b88001746ca
Eliza Adolfsson <eliza.adolfsson@gmail.com>
2020-12-08 16:58:53 +0100

    Småjusteringar och dummydata.

--- HotellDatabase/dbo/Tables/PaymentMethods.sql
+++ HotellDatabase/dbo/Tables/PaymentMethods.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[PaymentMethods]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Type] NVARCHAR(10) NULL
+    [Type] NVARCHAR(20) NULL
 )
--- HotellDatabase/dbo/Tables/Ratings.sql
+++ HotellDatabase/dbo/Tables/Ratings.sql
@@ -4,7 +4,7 @@
     [RoomId] INT NOT NULL, 
     [CustomerId] INT NOT NULL, 
     [Rating] INT NULL, 
-    [description] NTEXT NULL, 
+    [Description] NTEXT NULL, 
     CONSTRAINT [FK_Ratings_ToRooms] FOREIGN KEY ([RoomId]) REFERENCES [Rooms]([Id]), 
     CONSTRAINT [FK_Ratings_ToCustomers] FOREIGN KEY ([CustomerId]) REFERENCES [Customers]([Id])
 )
