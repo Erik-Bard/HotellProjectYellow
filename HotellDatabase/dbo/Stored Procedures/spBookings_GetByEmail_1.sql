e5973ec0eff2114b7550035c53cc169d9942f8b7
EricJohanzon <73117483+EricJohanzon@users.noreply.github.com>
2020-12-16 23:34:31 +0100

    Procedure1

---
+++ HotellDatabase/dbo/Stored Procedures/spBookings_GetByEmail_1.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE PROCEDURE dbo.spBookings_GetByEmail
+	@Email NVARCHAR(50)
+AS
+BEGIN
+SET NOCOUNT ON
+
+	SELECT Email, FirstName, LastName, BookingDate, BookedFrom, BookedTo, InvoiceId
+	FROM Customers
+	INNER JOIN Bookings ON Customers.Id = Bookings.CustomerId
+
+	WHERE Email = @Email
+END
