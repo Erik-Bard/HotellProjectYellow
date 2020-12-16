f50d7d017e9c12ba14c7de87aa627f0382969032
EricJohanzon <73117483+EricJohanzon@users.noreply.github.com>
2020-12-17 00:08:15 +0100

    Stored Procedure Updates

---
+++ HotellDatabase/dbo/Stored Procedures/spInvoice_Paid.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE PROCEDURE dbo.spInvoice_Paid
+	@Email NVARCHAR(50)
+AS
+BEGIN
+SET NOCOUNT ON
+
+	SELECT  Email, FirstName, LastName, Paid
+	FROM Customers
+	INNER JOIN Bookings ON Bookings.CustomerId = Customers.Id
+
+	INNER JOIN Invoices ON Invoices.Id = Bookings.Id
+
+	WHERE Email = @Email
+END
