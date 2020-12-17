385b1d0c8b7119707d1cfc6623b09768138f2cc6
EricJohanzon <73117483+EricJohanzon@users.noreply.github.com>
2020-12-17 18:44:07 +0100

    Added Transactions

--- HotellDatabase/dbo/Stored Procedures/sp_BookRoom.sql
+++ HotellDatabase/dbo/Stored Procedures/sp_BookRoom.sql
@@ -1,6 +1,50 @@
-﻿CREATE PROCEDURE [dbo].[sp_BookRoom]
-	@param1 int = 0,
-	@param2 int
+﻿CREATE PROCEDURE dbo.sp_BookRoom
+
+	/* Customer */
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@Email nvarchar(50),
+	@Phone nvarchar(15),
+	@Street nvarchar(50),
+	@Zip nvarchar(10),
+	@City nvarchar(50),
+	@Country nvarchar(50),
+
+	/* Bookings */
+	@BookingDate nvarchar(50),
+	@BookedFrom nvarchar(50),
+	@BookedTo nvarchar(50),
+	@Request nvarchar(200),
+	@TotalAmount money,
+	@NoShow bit,
+
+	/* BookingRows */
+	@RoomId int,
+	@NoGuests bit,
+	@NoExtraBeds bit,
+	@Price money,
+	@Discount money
+
+
 AS
-	SELECT @param1, @param2
-RETURN 0
+BEGIN TRANSACTION [Tran1]
+BEGIN TRY
+
+	INSERT INTO Customers(FirstName, LastName, Email, Phone, Street, Zip, City, Country)
+	VALUES (@FirstName, @LastName, @Email, @Phone, @Street, @Zip, @City, @Country)
+
+	INSERT INTO Bookings(BookingDate, BookedFrom, BookedTo, CustomerId, Request, TotalAmount, NoShow)
+	VALUES (@BookingDate, @BookedFrom, @BookedTo, SCOPE_IDENTITY() ,@Request, @TotalAmount, @NoShow)
+
+	INSERT INTO BookingRows(BookingId, RoomId, NoGuests, NoExtraBeds, CheckIn, CheckOut, Price, Discount )
+	VALUES (SCOPE_IDENTITY(), @RoomId, @NoGuests, @NoExtraBeds, @BookedFrom, @BookedTo, @Price, @Discount )
+
+
+	COMMIT TRANSACTION [Tran1]
+END TRY
+
+BEGIN CATCH
+		PRINT 'Transaction failed, rolling back'
+      ROLLBACK TRANSACTION [Tran1]
+
+END CATCH  
--- HotellDatabase/dbo/Stored Procedures/sp_DeleteBooking.sql
+++ HotellDatabase/dbo/Stored Procedures/sp_DeleteBooking.sql
@@ -1,6 +1,28 @@
-﻿CREATE PROCEDURE [dbo].[sp_DeleteBooking]
-	@param1 int = 0,
-	@param2 int
+﻿CREATE PROCEDURE dbo.sp_DeleteBooking
+	@Email nvarchar(50)
+
 AS
-	SELECT @param1, @param2
-RETURN 0
+BEGIN TRANSACTION [Tran1]
+BEGIN TRY
+
+	IF (@Email  is null or @Email = '')
+	BEGIN
+		RAISERROR('Invalid input',18, 0)
+		RETURN
+	END
+
+	IF CHARINDEX('@',@Email) = 0   
+	BEGIN  
+		RAISERROR('Input must contain @',18, 0)
+		RETURN
+	END  
+
+	DELETE Customers WHERE Email = @Email
+	COMMIT TRANSACTION [Tran1]
+END TRY
+
+BEGIN CATCH
+	PRINT 'Transaction Failed'
+      ROLLBACK TRANSACTION [Tran1]
+
+END CATCH  
