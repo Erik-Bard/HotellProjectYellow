aaf747493252c94a9bb394e51affb2964173b462
EricJohanzon <73117483+EricJohanzon@users.noreply.github.com>
2020-12-17 13:27:43 +0100

    Added data validation

--- HotellDatabase/dbo/Stored Procedures/spBookings_GetByEmail_1.sql
+++ HotellDatabase/dbo/Stored Procedures/spBookings_GetByEmail_1.sql
@@ -4,8 +4,20 @@ CREATE PROCEDURE dbo.spBookings_GetByEmail
 AS
 BEGIN
 SET NOCOUNT ON
+	
+	IF (@Email  is null or @Email = '')
+	BEGIN
+		RAISERROR('Invalid input',18, 0)
+		RETURN
+	END
 
-	SELECT Email, FirstName, LastName, BookingDate, BookedFrom, BookedTo
+	IF CHARINDEX('@',@Email) = 0   
+	BEGIN  
+		RAISERROR('Input must contain @',18, 0)
+		RETURN
+	END  
+
+	SELECT Email, FirstName, LastName, BookingDate, BookedFrom, BookedTo, Request, TotalAmount
 	FROM Customers
 	INNER JOIN Bookings ON Customers.Id = Bookings.CustomerId
 
